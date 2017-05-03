7a9cc02c226476cf59c458159a55c1b4efbf8620
laveh2107 <laveh2107@HRH-IC0037387.wrdsb.ca>
2017-05-03 13:17:02 -0400

    MSIP May 3rd

--- src/A7Q5.java
+++ src/A7Q5.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +12,49 @@
  */
 public class A7Q5 {
 
+    public static int chaotic(int lines) {
+        //Create for loop to run through the values leading up to the value of the lines
+        for (int i = 1; i <= lines; i++) {
+
+            //Do the math random method for random numbers from 1-5
+            int randNum = (int) (Math.random() * (5 - 1 + 1)) + 1;
+            
+            //Print out the number of asterisks according to the random number 
+            if (randNum == 1) {
+                System.out.println("*");
+            }
+            if (randNum == 2) {
+                System.out.println("**");
+            }
+            if (randNum == 3) {
+                System.out.println("***");
+            }
+            if (randNum == 4) {
+                System.out.println("****");
+            }
+            if(randNum == 5){
+                System.out.println("*****");
+            }
+        }
+        return 0;
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create input scanner
+        Scanner input = new Scanner(System.in);
+
+        //Ask the user for number of lines
+        System.out.println("Please enter the number of lines");
+        
+        //Create avriable lines for number of lines
+        int lines = input.nextInt();
+        
+        //Put lines inside the method
+        chaotic(lines);
+        
     }
 }
