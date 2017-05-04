741fe3912a9c04d1a2e0a79034e71361c34759de
laveh2107 <laveh2107@HRH-IC0027328.wrdsb.ca>
2017-05-04 10:00:09 -0400

    School, Nate's Computer, May the 4th be with you

--- src/A7Q6.java
+++ src/A7Q6.java
@@ -1,18 +1,41 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
- *
+ *Find the last digit of a number
  * @author laveh2107
  */
 public class A7Q6 {
 
     /**
+     * To find the remainder of the number divided by ten
+     *
+     * @param num the number one needs the last digit of
+     * @return The last digit
+     */
+    public static int lastDigit(int num) {
+
+        //find the remainer of the number divided by ten which is the first digit
+        int x = num % 10;
+        return x;
+    }
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Insert Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Ask user for the number
+        System.out.println("Please enter the integer you wish to find the last digit of");
+        int num = input.nextInt();
+
+        //Have the number run through the method
+        System.out.println("The last digit of " + num + " is: " + lastDigit(num));
     }
 }
