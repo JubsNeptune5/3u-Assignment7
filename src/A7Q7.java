a579bd33929f4bd69211c4a46d6724223ca7c633
laveh2107 <laveh2107@HRH-IC0037387.wrdsb.ca>
2017-05-04 14:04:50 -0400

    MSIP May 4rd

--- src/A7Q7.java
+++ src/A7Q7.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +12,23 @@
  */
 public class A7Q7 {
 
+    public static int firstDigit(int num){
+        return 0;
+        
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Insert Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Ask user for the number
+        System.out.println("Please enter the integer you wish to find the first digit of");
+        int num = input.nextInt();
+
+        //Have the number run through the method
+        System.out.println("The last digit of " + num + " is: " + firstDigit(num));
     }
 }
